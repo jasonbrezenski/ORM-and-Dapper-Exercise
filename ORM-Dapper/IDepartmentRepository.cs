a893b574756c289ff98b70ad65f2fe1fee9c9357
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-09-16 11:45:17 -0500

    Exercise 1 Completed

---
+++ ORM-Dapper/IDepartmentRepository.cs
@@ -0,0 +1,7 @@
+namespace ORM_Dapper;
+
+public interface IDepartmentRepository
+{
+    public IEnumerable<Department> GetAllDepartments();
+    public void InsertDepartment(string newDepartmentName);
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using ORM_Dapper;
+
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+//departmentRepo.InsertDepartment("CSharp-50");  //If keep running, will keep adding CSharp-50 as #6/#7/38/etc so comment out after running.
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach (var dept in departments)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine($"DepartmentID: {dept.DepartmentID} | Name: {dept.Name}");
 }
+
+
